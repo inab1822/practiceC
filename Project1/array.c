c2fc6f29f435e6c6115019186170999fc86fa1ef
관리자 <@DESKTOP-7S1QOCC>
2023-07-12 14:29:55 +0900

    Add array_project.c file

--- Project1/array.c
+++ Project1/array.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main(void)
+int main_array(void)
 {
 	//// 배열
 	//int subway_1 = 30; // 지하철 1호선에는 30명이 타고있다.
@@ -55,6 +55,90 @@ int main(void)
 		printf("%.2f\n", arr_f[i]);
 	}
 	*/
+
+	// 문자 vs 문자열
+	/*char c = 'A';
+	printf("%c", c);*/
+
+	// 문자열 끝에는 '끝'을 의미하는 NULL 문자 '\0' 이 포함되어야 함
+	// 안그러면 출력시 coding 뒤에 이상한 문자가 같이 출력된다.
+	//char str[6] = "coding"; // [c][o][d][i][n][g][\0]
+
+	/*char str[7] = "coding";
+	printf("%s\n", str);*/
+
+	//char str[] = "coding";
+	////printf("%s\n", str);
+	////printf("%d\n", sizeof(str)); // sizeof()는 해당 배열의 크기
+
+	//for (int i = 0; i < sizeof(str); i++)
+	//{
+	//	printf("%c\n", str[i]);
+	//}
+
+	//char kor[] = "나도코딩";
+	//printf("%s\n", kor);
+	//printf("%d\n", sizeof(kor)); // 실행하면 kor의 크기가 9라고 나옴
+	//// 영어 한글자 : 1 byte
+	//// 한글 한글자 : 2 byte
+	//// char 크기 : 1 byte
+	//
+
+	//char c_array[7] = { 'c','o','d','i','n','g','\0'}; // 제대로 출력됨
+	//char c_array[6] = { 'c','o','d','i','n','g'}; // '\0' 이 들어갈 자리가 없어서 이상한 값이 출력
+	//printf("%s\n", c_array);
+	//
+
+	//char c_array[10] = { 'c','o','d','i','n','g' };
+	//printf("%s\n", c_array);
+	/*for (int i = 0; i < sizeof(c_array); i++)
+	{
+		printf("%c\n", c_array[i]);
+	}*/
+
+	//for (int i = 0; i < sizeof(c_array); i++)
+	//{
+	//	printf("%d\n", c_array[i]); // ASCII 코드 값 (null 문자는 0으로 입력됨)
+	//}
+
+	// 문자열 입력받기
+	/*char name[256];
+	
+	printf("이름을 입력하세요. : ");
+	scanf_s("%s", name, sizeof(name));
+	printf("%s", name);*/
+
+	
+	// 참고 : ASCII 코드? ANSI(미국표준협회) 에서 제시한 표준 코드 체계
+	// 7bit, 총 128개 코드 (0 ~ 127)
+	// a : 97 (문자 a의 아스키코드 정수값)
+	// A : 65
+	// 0 : 48
+	
+	/*printf("%c\n", 'a');
+	printf("%d\n", 'a');
+
+	printf("%d\n", 'b');
+	printf("%d\n", 'b');
+
+	printf("%d\n", 'A');
+	printf("%d\n", 'A');
+
+	printf("%d\n", '\0');
+	printf("%d\n", '\0');
+
+	printf("%d\n", '0');
+	printf("%d\n", '0');
+
+	printf("%d\n", '1');
+	printf("%d\n", '1');*/
+
+	// 참고2 : 0 ~ 127 사이의 아스키코드 정수값에 해당하는 문자 확인
+	for (int i = 0; i < 128; i++)
+	{
+		printf("아스키코드 정수 %d : %c\n", i, i);
+	}
+
 
 	return 0;
 }
